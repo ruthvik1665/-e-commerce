2d73e84750d9e736a66eb5b119cf24f69803ea23
Ruthvik Basetti <ruthvikbasetty5@gmail.com>
2024-06-22 14:13:34 +0530

    Commited after adding of home page,with product list,pagination

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+import HomePage from './Pages/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,11 @@
+import Navbar from "../features/Navbar/navbar";
+import ProductList from "../features/product-list/ProductList";
+
+function HomePage() {
+    return (<Navbar>
+        <ProductList>
+        </ProductList>
+    </Navbar>);
+}
+
+export default HomePage;
