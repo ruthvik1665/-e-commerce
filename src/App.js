2f75e80f37c7964c0f14cacc78b8919d25ffdde1
Ruthvik Basetti <ruthvikbasetty5@gmail.com>
2024-06-23 19:56:42 +0530

    added signup and login pages with react routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,38 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import HomePage from './Pages/Homepage';
+import LoginPage from './Pages/LoginPage';
+import Signup from './Pages/SignupPage';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <HomePage></HomePage>
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: <><LoginPage></LoginPage></>,
+  }, {
+    path: "/signup",
+    element: <><Signup></Signup></>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,6 @@
+import Login from "../features/auth/components/Login";
+function LoginPage() {
+    return (<><Login></Login></>);
+}
+
+export default LoginPage;
